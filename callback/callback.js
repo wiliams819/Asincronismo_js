3eba9ae36ac4d6e84905fa118347a20213eda407
Wiliams Ixcoy <wiliamsg200295@gmail.com>
2021-06-26 16:09:32 -0700

    primer ejercicio con callback falta comentar

--- callback/callback.js
+++ callback/callback.js
@@ -1,9 +1,35 @@
-function suma(sum1, sum2){
-    return num1 +num2;
+function suma(num1, num2){
+
+    return num1 + num2;
 }
 
 function calc( num1, num2, callback){
    return callback(num1, num2);
 }
 
-console.log(calc(2,5, suma));
+document.write(calc(2,6, suma));
+
+
+
+
+
+
+
+
+
+function date(callback){
+    console.log(new Date);
+
+
+    setTimeout(function (){
+        let dato = new Date
+        callback(dato);
+    }, 3000)
+}
+
+
+function mostrar(Nuevodato){
+  console.log(Nuevodato);
+  
+}
+date(mostrar);
